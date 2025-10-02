dfbf1fbb371bddb40f98b276cf94d4efff9c9d88
Ddyadz01 <albertmik2002@gmail.com>
2025-10-03 00:44:30 +0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Confetti from 'react-confetti-boom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Confetti particleCount={80} />
   </React.StrictMode>,
-);
+)
