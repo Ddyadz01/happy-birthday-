67948da1eee0a5ffc73b701f327ce67c637098a1
Ddyadz01 <albertmik2002@gmail.com>
2025-10-03 12:03:19 +0300

    FIx

--- src/App.jsx
+++ src/App.jsx
@@ -81,57 +81,69 @@ export default function App() {
             <span>ВС</span>
           </div>
           <div className="calendar__row">
+            <span className="prev-day">29</span>
+            <span className="prev-day">30</span>
             <span>1</span>
             <span>2</span>
             <span>3</span>
             <span>4</span>
             <span>5</span>
-            <span>6</span>
-            <span>7</span>
           </div>
           <div className="calendar__row">
+            <span>6</span>
+            <span>7</span>
             <span>8</span>
             <span>9</span>
             <span>10</span>
             <span>11</span>
             <span>12</span>
-            <span>13</span>
-            <span>14</span>
           </div>
           <div className="calendar__row">
+            <span>13</span>
+            <span>14</span>
             <span>15</span>
             <span>16</span>
             <span>17</span>
             <span>18</span>
             <span>19</span>
-            <span>20</span>
-            <span>21</span>
           </div>
           <div className="calendar__row">
+            <span>20</span>
+            <span>21</span>
             <span>22</span>
             <span>23</span>
             <span>24</span>
             <span className="calendar__day--accent">25</span>
             <span>26</span>
-            <span>27</span>
-            <span>28</span>
           </div>
           <div className="calendar__row">
+            <span>27</span>
+            <span>28</span>
             <span>29</span>
             <span>30</span>
             <span>31</span>
-            <span></span>
-            <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
       </section>
       <section className="location-block">
-        <h1>Сохраните место</h1>
+        <h1 className="hero__title">Сохраните место</h1>
         <div className="location">
           <h2 className="location__place">Ресторан «Татев»</h2>
-          <p className="location__addr">д. Путилково, 11, стр. 17</p>
+          <h4 className="location__addr">Адрес: д. Путилково, 11, стр. 17</h4>
+          <h3 className="location__place">Сбор гостей в 17:00</h3>
+
+          <img src="restoran.webp" alt="" />
+          <div className="navigator">
+            <a
+              href="https://yandex.ru/maps/?rtext=~55.861509  37.397530"
+              target="_blank"
+              className="uk-button uk-button-primary"
+            >
+              <button>Как добраться?</button>
+            </a>
+          </div>
         </div>
         <a className="hero__scroll" href="#rsvp">
           ﹀
