a3a7bdc3a27b5c726eca488d4f6b4ccad43f8400
Ddyadz01 <albertmik2002@gmail.com>
2025-10-03 15:09:07 +0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Confetti from 'react-confetti-boom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Confetti particleCount={800} colors={['#f3856a', '#ffc14d', '#28e791']} />
     <App />
+    <Confetti particleCount={800} colors={['#f3856a', '#ffc14d', '#28e791']} />
   </React.StrictMode>,
 )
